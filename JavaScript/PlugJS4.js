2f5c184de9a7ab61e60839e644e55220f9e81a05
conghoan21 <conghoan.hnc@gmail.com>
2020-08-11 23:35:03 +0700

    add file

--- JavaScript/PlugJS4.js
+++ JavaScript/PlugJS4.js
@@ -2,6 +2,6 @@ function convert() {
     let amount = document.getElementById("amount").value;
     let from = document.getElementById("from").value;
     let to = document.getElementById("to").value;
-    let result = amount*from/to;
-    document.getElementById("result").innerHTML= 'result: '+result
+    let result = amount * from / to;
+    document.getElementById("result").innerHTML = 'result : ' + result;
 }
