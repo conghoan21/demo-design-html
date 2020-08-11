bb704305c0923efc8346b882d14af164221ca4a9
conghoan21 <conghoan.hnc@gmail.com>
2020-08-11 16:02:02 +0700

    add file

---
+++ JavaScript/PlusJs3.js
@@ -0,0 +1,6 @@
+let inputWidth=prompt('Enter the Width');
+let inputHeight=prompt('Enter the Height');
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area=inputWidth*inputHeight
+document.write('' +area)
